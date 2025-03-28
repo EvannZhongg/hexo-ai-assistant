d0297d034e37b91e8b7fba4662c134a8be968469
EvannZhongg <freakthrow@gmail.com>
2025-03-28 22:29:35 +0800

    Update auto_vector.js

--- auto_vector.js
+++ auto_vector.js
@@ -4,7 +4,7 @@ hexo.extend.filter.register('before_generate', function() {
   console.log("🎯 正在构建向量库...");
 
   return new Promise((resolve, reject) => {
-    exec("D:/Personal_Project/HexoAgent/.venv/Scripts/python D:/Personal_Project/HexoAgent/build_vector_store.py",
+    exec("/your_project/.venv/Scripts/python build_vector_store.py", {
       { cwd: "D:/Blog" },
       (err, stdout, stderr) => {
         if (err) {
